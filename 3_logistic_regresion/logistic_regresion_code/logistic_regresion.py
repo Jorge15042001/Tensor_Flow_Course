c152c3eb541df65eed36672eb481619688eb8301
Jorge15042001 <adrivulgarin@hotmail.com>
2021-03-15 01:46:32 -0500

    logistic regrsion

---
+++ 3_logistic_regresion/logistic_regresion_code/logistic_regresion.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import time
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+#load de dataset
+iris_datset = load_iris()#iris is a dictionary, that have data and traget as keys
+
+print ( type ( iris_datset ) )
+iris_imput_data = iris_datset.data # take the input data
+
+iris_output_data = iris_datset.target # take the labels 
+#print(iris_imput_data)
+iris_output_data = pd.get_dummies( iris_output_data ) # separates the data into 3 columns one for each class, puts 1 where the class was defined and 0 in the rest
+# [1, 2, 1, 2, 3]-> becomes 
+#[[1, 0, 1, 0, 0],
+# [0, 1, 0, 1, 0],
+# [0, 0, 0, 0, 1]]
+
+
+train_x, test_x, train_y, test_y = train_test_split(
+        iris_imput_data, 
+        iris_output_data, 
+        test_size= 0.33, 
+        random_state= 1)
+
+numbers_of_features = train_x.shape[1] # dimentionality of the input vector, aka: number of independent variables, aka: number of variables for each input
+print("Iris_data_set constains ", numbers_of_features ," features")
+numbers_of_labels = train_y.shape[1] # dimentionality of the output vector, aka: numbr of classes that can be predicted
+print ("Iris_data_set have "+ numbers_of_labels + " valid classes")
+
+
+# using tensorflow 
+
+# make the data set a tensorflow constant
+tf_train_x = tf.constant ( train_x, dtype = 'float32' )
+tf_train_y = tf.constant ( train_y, dtype = 'float32' )
+tf_test_x = tf.constant ( test_x, dtype = 'float32' )
+tf_test_y = tf.constant ( test_y, dtype = 'float32' )
+
+# defintion of trainable variables 
+
+model_weights = tf.Variable (tf.zeros([4, 3]))
+model_biases = tf.Variable (tf.zeros ([3])) 
+
+
+
+
